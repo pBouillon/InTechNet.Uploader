4f50ae3d006701532dbb668929e5e31657f78d35
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-04-17 01:22:08 +0200

    ✨ Create module from CLI

--- uploader.py
+++ uploader.py
@@ -1,9 +1,33 @@
+from configparser import ConfigParser
 import click
 from pathlib import Path
+import psycopg2
 from typing import List
 import yaml
 
 from utils.dataclasses import Module, Resource
+
+
+def extract_config(filename='database.ini', section='postgresql'):
+    '''Extract all configuration information'''
+    # From: https://www.postgresqltutorial.com/postgresql-python/connect/
+    # Create a parser
+    parser = ConfigParser()
+    
+    # Read config file
+    parser.read(filename)
+
+    # Get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
 
 
 def get_resources(path: Path) -> List[Resource]:
@@ -72,6 +96,31 @@ def get_module_data(module_file: Path) -> Module:
     return Module(**data['module'])
 
 
+def record_module(module: Module, subscription_plan_id: int, config: dict = extract_config()):
+    '''Record the provided module in database'''
+    
+    sql = '''
+       INSERT INTO "module" ("ModuleDescription", "SubscriptionPlanId", "ModuleName")
+       VALUES(%s, %s, %s);
+    '''
+    
+    # From: https://www.postgresqltutorial.com/postgresql-python/insert/
+    conn = None
+    try:
+        # Connect to the PostgreSQL database
+        conn = psycopg2.connect(**config)
+        # Create a new cursor
+        cur = conn.cursor()
+        # Execute the INSERT statement
+        cur.execute(sql, (module.description, subscription_plan_id, module.name))
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the database
+        cur.close()
+    finally:
+        if conn is not None:
+            conn.close()
+
 @click.command()
 @click.argument('path', type=click.Path(exists=True))
 @click.option(
@@ -80,7 +129,14 @@ def get_module_data(module_file: Path) -> Module:
     'module',
     help='Path to the module description file',
     type=click.Path(exists=True))
-def upload(path, module):
+@click.option(
+    '--subscriptionplan',
+    '-s',
+    'subscription_plan_id',
+    default=1,
+    help='Id of the subscription plan to which this module is intended',
+    type=int)
+def upload(path, module, subscription_plan_id):
     '''Upload a module and its resources in the correct order'''
     root_path = Path(path)
     
@@ -97,6 +153,8 @@ def upload(path, module):
     # Bind the first resource to the module
     module.first_resource = resources_head
 
+    # Add a new record for the module
+    record_module(module, subscription_plan_id)
 
 if __name__ == '__main__':
     upload()
